e839dc442d366d5783f45bf67dd8cf48c0dc3164
Artur Zaczek <artur200212@interia.pl>
2022-10-06 12:14:22 +0200

    added routes for get methods

--- Server/Routes/Word.ts
+++ Server/Routes/Word.ts
@@ -1,5 +1,9 @@
 import Express from "express"
+import { getCategories, getSubCategories } from "../Controllers/Word";
 
 export const wordRouter = Express.Router();
+
+wordRouter.get('/categories', getCategories);
+wordRouter.get('/sub-categories/:categoryId', getSubCategories);
 
 wordRouter.use((req, res) => res.status(404).end())
