7b47e2b433e13d21cd91f8263df4f89558a3e39b
Artur Zaczek <artur200212@interia.pl>
2022-10-06 12:18:29 +0200

    creating menu component

---
+++ Client/src/component/Menu/Menu.tsx
@@ -0,0 +1,21 @@
+import * as React from 'react';
+import { useGetCategoriesQuery } from '../../Redux/Services/learnLangApi';
+import CategoryRow from '../CategoryRow/CategoryRow';
+
+import './Menu.scss'
+
+const Menu = () => {
+
+    const { data, error, isLoading } = useGetCategoriesQuery(null)
+    console.log(data);
+
+    const renderCategories = data?.data.map(({ name }: { name: string }) => <CategoryRow key={name} page='/category' titleText={name} />);
+
+    return (
+        <section>
+            {renderCategories}
+        </section>
+    );
+}
+
+export default Menu;
