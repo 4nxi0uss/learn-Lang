fa36dc2e9815ea3a2d7bbb83db4f32902066ad65
Artur Zaczek <artur200212@interia.pl>
2022-10-06 12:32:07 +0200

    changing layout

--- Client/src/component/AddSth/AddSth.tsx
+++ Client/src/component/AddSth/AddSth.tsx
@@ -1,19 +1,12 @@
-import Style from './AddSth.module.scss'
-import Block from 'bem-css-modules'
-
-const b = Block(Style)
+import './AddSth.scss'
 
 const AddSth = () => {
 
-
     return (
-        <section className={b()} onClick={(e) => {
-            console.log(e.target);
-        }}>
-            <div className={b('horizontal')}></div>
-
-            <div className={b('vertical')}></div>
-            <div className={b('addText')}>Dodaj!</div>
+        <section className='add' onClick={(e) => { console.log(e.target); }}>
+            <div className='add__horizontal'></div>
+            <div className='add__vertical'></div>
+            <div className='add__addText'>Dodaj!</div>
         </section>
     );
 }
