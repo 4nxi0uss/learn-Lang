ee920a83757174e1f1780639024e5eab6daa4d14
Artur Zaczek <artur200212@interia.pl>
2022-10-06 12:20:03 +0200

    added navigation route to header title

--- Client/src/component/Header/Header.tsx
+++ Client/src/component/Header/Header.tsx
@@ -1,9 +1,13 @@
+import { useNavigate } from 'react-router';
+
 import './Header.scss'
 
 const Header = () => {
+    const navigate = useNavigate()
+
     return (
         <header className='header'>
-            <h1 className='header__title'>LearnLang</h1>
+            <h1 className='header__title' onClick={() => { navigate('/') }}>LearnLang</h1>
         </header>
     );
 }
