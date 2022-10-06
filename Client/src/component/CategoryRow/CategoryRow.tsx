f293dfe012284eefc63eebe1a0dbb6ba659428e0
Artur Zaczek <artur200212@interia.pl>
2022-10-06 12:29:46 +0200

    added navigation and dipatch categoryName

--- Client/src/component/CategoryRow/CategoryRow.tsx
+++ Client/src/component/CategoryRow/CategoryRow.tsx
@@ -1,15 +1,17 @@
-import { useLocation, useNavigate } from 'react-router'
+import { useNavigate } from 'react-router'
+import { categoryName } from '../../Redux/Slice/categorySlice';
+import { useAppDispatch } from '../../Redux/Hooks/Hooks';
 
 import './CategoryRow.scss'
 
+const CategoryRow = ({ titleText, page }: { titleText: string, page: string }) => {
 
-const CategoryRow = ({ titleText }: { titleText: string }) => {
+    const dispatch = useAppDispatch()
 
-    const Location = useLocation();
     const navigate = useNavigate();
 
     return (
-        <section className='category' onClick={() => { console.log(titleText); console.log(Location); navigate(titleText) }}>
+        <section className='category' onClick={() => { console.log(titleText); navigate(page); dispatch(categoryName(titleText)) }}>
             <h2 className='category__title'>{titleText}</h2>
         </section>
     );
