a69f22ff851510ff81345bbce39a5ce69823ddfd
Artur Zaczek <artur200212@interia.pl>
2022-10-13 22:20:58 +0200

    add two routes, /PL-ENG and /post-words

--- Server/Routes/Word.ts
+++ Server/Routes/Word.ts
@@ -1,9 +1,11 @@
 import Express from "express"
-import { getCategories, getSubCategories } from "../Controllers/Word";
+import { getCategories, getSubCategories, patchWordPLAndENG, postWords } from "../Controllers/Word";
 
 export const wordRouter = Express.Router();
 
 wordRouter.get('/categories', getCategories);
 wordRouter.get('/sub-categories/:categoryId', getSubCategories);
+wordRouter.patch('/PL-ENG', patchWordPLAndENG);
+wordRouter.post('/post-words', postWords);
 
 wordRouter.use((req, res) => res.status(404).end())
