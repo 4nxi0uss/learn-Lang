b28a3697bd70e7e21d937a17f9dd7169345801c5
Artur Zaczek <artur200212@interia.pl>
2022-10-13 22:47:34 +0200

    added navigation to add component

--- Client/src/component/AddSth/AddSth.tsx
+++ Client/src/component/AddSth/AddSth.tsx
@@ -1,9 +1,13 @@
+import { useNavigate } from 'react-router';
+
 import './AddSth.scss'
 
 const AddSth = () => {
 
+    const navigate = useNavigate()
+
     return (
-        <section className='add' onClick={(e) => { console.log(e.target); }}>
+        <section className='add' onClick={(e) => { console.log(e.target); navigate('/add') }}>
             <div className='add__horizontal'></div>
             <div className='add__vertical'></div>
             <div className='add__addText'>Dodaj!</div>
