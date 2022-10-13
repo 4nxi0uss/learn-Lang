267bb8acb36f2faadcdf14ed54175249b7ed0624
Artur Zaczek <artur200212@interia.pl>
2022-10-13 22:33:45 +0200

    added two new route

--- Client/src/component/MainContent/MainContent.tsx
+++ Client/src/component/MainContent/MainContent.tsx
@@ -5,6 +5,8 @@ import { Route, Routes } from 'react-router';
 import AddSth from '../AddSth/AddSth';
 import Category from '../Category/Category';
 import Menu from '../Menu/Menu';
+import SubCategory from '../SubCategory/SubCategory';
+import InputWordForm from '../inputWordForm/inputWordForm';
 
 const MainContent = () => {
 
@@ -13,7 +15,10 @@ const MainContent = () => {
             <Routes>
                 <Route path='/' element={<Menu />} />
                 <Route path='/category' element={<Category />} />
+                <Route path='/sub-category' element={<SubCategory />} />
+                <Route path='/add' element={<InputWordForm />} />
             </Routes>
+
             <AddSth />
         </main>
     );
